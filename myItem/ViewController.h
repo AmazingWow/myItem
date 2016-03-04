1787a5532ce290cf199966686863849fcfc509c5
scjy <111>
2016-03-04 10:12:40 +0800

    init

---
+++ myItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myItem
+//
+//  Created by wp on 16/3/4.
+//  Copyright © 2016年 wp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myItem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  myItem
+//
+//  Created by wp on 16/3/4.
+//  Copyright © 2016年 wp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 1.0已完成。
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
